3c84d5b99aa368cfee0cc7e3f09b4d9372867825
unknown <moacir.viana@gmail.com>
2021-01-10 21:24:37 -0400

    Add navigation gps e confirm order

---
+++ front-mobile/src/OrderDetails/Index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, Image, Alert, Linking } from 'react-native';
+import Header from '../Header/Index';
+import { Order } from '../types';
+import OrderCard from '../OrderCard/Index';
+import { RectButton } from 'react-native-gesture-handler';
+import { confirmConfirmDelivery } from '../api';
+
+type Props = {
+  route : {
+    params : {
+      order: Order;
+    }
+  }
+}
+
+function OrderDetails({route}:Props) {
+  const {order} = route.params;
+  const navigation = useNavigation();
+
+  const handleOnCancel = () => {
+    navigation.navigate('Orders')
+  }
+
+  const handleConfirmDelivey = () => {
+    confirmConfirmDelivery(order.id)
+        .then( () =>{
+          Alert.alert(`Pedido ${order.id} confirmado com sucesso!`);
+          navigation.navigate('Orders');
+        })
+        .catch( (error) =>{
+          console.log(error)
+          Alert.alert(`Houve um erro ao confirmar pedido ${order.id}`);
+        })
+  }
+
+  const handleStartNavigation = () =>{
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`)
+  }
+
+
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+          <OrderCard order={order} />
+          <RectButton style={styles.button} onPress={handleStartNavigation}>
+               <Text style={styles.buttonText}>Inicinar Navegação</Text>
+          </RectButton>
+          <RectButton style={styles.button} onPress={handleConfirmDelivey}>
+               <Text style={styles.buttonText}>Confirmar Entrega</Text>
+          </RectButton>
+          <RectButton style={styles.button} onPress={handleOnCancel}>
+               <Text style={styles.buttonText}>Cancelar</Text>
+          </RectButton>
+      </View>
+    </>  
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      paddingRight: '5%',
+      paddingLeft: '5%'
+    },
+    button: {
+      backgroundColor: '#DA5C5C',
+      flexDirection: 'row',
+      borderRadius: 10,
+      marginTop: 40,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    buttonText: {
+      paddingTop: 15,
+      paddingBottom: 15,
+      paddingLeft: 50,
+      paddingRight: 50,
+      fontWeight: 'bold',
+      fontSize: 18,
+      color: '#FFF',
+      letterSpacing: -0.24,
+      fontFamily: 'OpenSans_700Bold'
+    }
+  }
+);
+
+export default OrderDetails;
--- front-mobile/src/Orders/Index.tsx
+++ front-mobile/src/Orders/Index.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text} from 'react-native';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header/Index';
 import OrderCard from '../OrderCard/Index';
-
+import { Order } from '../types';
 
 function Orders() {
-  const handleOnPress = () => {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
 
-  }
+const fectchData = () =>{
+  setIsLoading(true);
+    fetchOrders()
+        .then( response => setOrders(response.data))
+        .catch( error => Alert.alert("Houve um erro ao buscar os pedidos"))
+        .finally(() => setIsLoading(false))
+}
+
+   useEffect ( () => {
+    if (isFocused){
+      fectchData();
+    }
+   }, [isFocused]);
+
+   const handleOnPress = (order : Order) => {
+      navigation.navigate('OrderDetails', {
+        order
+      });
+   }
 
   return (
     <>
      <Header />
       <ScrollView style={styles.container}>
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
-          <OrderCard />
+          {isLoading ? (
+            <Text >Buscando pedidos...</Text>
+          ) : (
+            orders.map( order => (
+              <TouchableWithoutFeedback 
+                 key={order.id} 
+                 onPress={()=> handleOnPress(order) }
+              >
+                   <OrderCard order={order} />
+              </TouchableWithoutFeedback>
+             ))
+          )}
       </ScrollView>
       
     </>
